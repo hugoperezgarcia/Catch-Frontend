e78e2c0b9e18b264818a1cebec85a881f6ddc6e0
Hugo Pérez García <hugo.perez@pcvivbeca01.mdnova.local>
2024-04-15 12:01:23 +0200

    pruebas con la base de datos

---
+++ src/hooks/usePartidas.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { UseUser } from "./UseUser";
+
+export default function usePartidas(){
+
+    const{user} = UseUser();
+    const [filtro, setFiltro] = useState("");
+    const [partidasFiltradas, setPartidasFiltradas] = useState([]);
+
+    const filtrarPartidas = (event) => {
+        setFiltro(event.target.value);
+    }
+
+    useEffect(() => {
+        setPartidasFiltradas(user.partidas.filter(partida =>
+            partida.titulo.toLowerCase().includes(filtro.toLowerCase())
+        ));
+    }, []);
+
+    return{filtrarPartidas, partidasFiltradas}
+}
