5c3c77f7ea03e72fd9dc46f30167a5701163309f
Hugo Pérez García <hugo.perez@pcvivbeca01.mdnova.local>
2024-04-25 13:30:15 +0200

    Ranking

--- src/components/Ranking.jsx
+++ src/components/Ranking.jsx
@@ -9,34 +9,32 @@ export function Ranking() {
                     <div className="h-2/4 flex justify-center items-center">
                         <div class="flex justify-center items-end space-x-0.5">
                             <div class="flex flex-col items-center">
+                                <p className="text-white">Hugo M.</p>
                                 <div class="bg-yellow-400 h-40 w-20 shadow-xl rounded-md"></div>
                                 <p class="text-center font-bold text-lg text-white">2º</p>
                             </div>
                             <div class="flex flex-col items-center">
+                                <p className="text-white">Hugo P.</p>
                                 <div class="bg-yellow-400 h-48 w-20 shadow-xl rounded-md"></div>
                                 <p class="text-center font-bold text-lg text-white">1º</p>
                             </div>
                             <div class="flex flex-col items-center">
+                                <p className="text-white">Elon Musk</p>
                                 <div class="bg-yellow-400 h-32 w-20 shadow-xl rounded-md"></div>
                                 <p class="text-center font-bold text-lg text-white">3º</p>
                             </div>
                         </div>
                     </div>
-                    <div className="h-2/4 flex flex-col items-center">
-                        <ol class="list-decimal flex flex-col items-start text-white">
-                            <li>Juan</li>
-                            <li>Pedro</li>
-                            <li>Pedro</li>
-                            <li>Pedro</li>
-                            <li>Pedro</li>
-                            <li>Pedro</li>
-                            <li>Pedro</li>
-                            <li>Pedro</li>
-                            <li>Pedro</li>
-                            <li>Pedro</li>
-                        </ol>
-                        <ul className="border-2 border-fuchsia-500 rounded-md p-1 w-11/12 flex justify-center text-white">
-                            <li>34.Tu posicion</li>
+                    <div className="h-2/4 flex flex-col items-center w-full">
+                        <ul class="flex flex-col items-center text-white w-4/5 bg-violet-800 rounded-xl">
+                            <li>4. Juan</li>
+                            <li>5. Pedro</li>
+                            <li>6. Pedro</li>
+                            <li>7. Pedro</li>
+                            <li>8. Pedro</li>
+                            <li>9. Pedro</li>
+                            <li>10. Pedro</li>
+                            <li className="border-4 border-yellow-500 rounded-md p-1 flex justify-center text-white w-full">34. Tu posicion</li>
                         </ul>
                     </div>
                 </body>
